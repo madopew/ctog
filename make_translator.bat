a908edf6be37aa20fc62aa772f4c615d4bbaedf4
Bakyt Madi <bakyt.madi.work@gmail.com>
2020-10-30 21:32:21 +0300

    Add echo messages

--- make_translator.bat
+++ make_translator.bat
@@ -1,4 +1,10 @@
 @echo off
+echo generating lex...
 flex c_lexer.l
+echo lex generated.
+echo generating parser...
 bison -d c_parser.y
-gcc -Wall -std=c99 c_parser.tab.c lex.yy.c -o translator.exe
+echo parser generated.
+echo compiling translator...
+gcc -Wall -std=c11 c_parser.tab.c lex.yy.c -o translator.exe
+echo translator compiled.
