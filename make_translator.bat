981504072a06f39e55aa532fcb30c7c02d868585
Bakyt Madi <bakyt.madi.work@gmail.com>
2020-11-01 18:22:59 +0300

    Add exit on error

--- make_translator.bat
+++ make_translator.bat
@@ -1,10 +1,13 @@
 @echo off
 echo generating lex...
 flex c_lexer.l
+if %errorlevel% neq 0 exit /b %errorlevel%
 echo lex generated.
 echo generating parser...
 bison -d c_parser.y
+if %errorlevel% neq 0 exit /b %errorlevel%
 echo parser generated.
 echo compiling translator...
 gcc -Wall -std=c11 c_parser.tab.c lex.yy.c -o translator.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
 echo translator compiled.
