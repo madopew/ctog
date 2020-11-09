bbe2c66aa8fe01c273abd2e1a81d96d3e728722f
Bakyt Madi <bakyt.madi.work@gmail.com>
2020-11-09 15:41:11 +0300

    Remove unnecessary files

--- ctox/make_translator.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo generating parser...
-bison -d c_parser.y
-if %errorlevel% neq 0 exit /b %errorlevel%
-echo parser generated.
-echo generating lex...
-flex c_lexer.l
-if %errorlevel% neq 0 exit /b %errorlevel%
-echo lex generated.
